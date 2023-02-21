a67bbb47158facd403da4bc0672d387ae1a84455
Aniket Kadam <aniketa2k@gmail.com>
2023-02-21 16:45:41 +0530

    updated name to fullname

--- GitBasics/Employee.cs
+++ GitBasics/Employee.cs
@@ -8,7 +8,7 @@ namespace GitBasics
     {
         public int Id { get; set; }
 
-        public string Name { get; set; }
+        public string FullName { get; set; }
         
         public string Address { get; set; }
     }
